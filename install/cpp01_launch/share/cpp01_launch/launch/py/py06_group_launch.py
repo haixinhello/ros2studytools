21d651ebfd84da13d675458b6fa76c205593a8fa
haixin <haixinhello@gmail.com>
2025-08-12 00:18:56 +0800

    python_launch

---
+++ install/cpp01_launch/share/cpp01_launch/launch/py/py06_group_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+# 封装终端指令相关类--------------
+# from launch.actions import ExecuteProcess
+# from launch.substitutions import FindExecutable
+# 参数声明与获取-----------------
+# from launch.actions import DeclareLaunchArgument
+# from launch.substitutions import LaunchConfiguration
+# 文件包含相关-------------------
+# from launch.actions import IncludeLaunchDescription
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# 分组相关----------------------
+from launch_ros.actions import PushRosNamespace
+from launch.actions import GroupAction
+# 事件相关----------------------
+# from launch.event_handlers import OnProcessStart, OnProcessExit
+# from launch.actions import ExecuteProcess, RegisterEventHandler,LogInfo
+# 获取功能包下 share 目录路径-------
+# from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    #创建三个turtlesim节点
+    t1=Node(package="turtlesim",executable="turtlesim_node",name="t1")
+    t2=Node(package="turtlesim",executable="turtlesim_node",name="t2")
+    t3=Node(package="turtlesim",executable="turtlesim_node",name="t3")
+    #分组
+    g1=GroupAction(actions=[PushRosNamespace("g1"),t1,t2])
+    g2=GroupAction(actions=[PushRosNamespace("g2"),t3])
+    return LaunchDescription([g1,g2])
