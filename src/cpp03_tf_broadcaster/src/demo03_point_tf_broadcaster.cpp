d20e6c516a5d5d7de9ce09b7a6b4b1180da9fd5e
haixin <haixinhello@gmail.com>
2025-08-17 09:46:34 +0800

    point_broadcaster

---
+++ src/cpp03_tf_broadcaster/src/demo03_point_tf_broadcaster.cpp
@@ -0,0 +1,35 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+using namespace std::chrono_literals;
+class PointBroadcaster: public rclcpp::Node
+{
+public:
+    PointBroadcaster():Node("point_broadcaster_node_cpp"),x(0.0)
+    {
+        point_pub_=this->create_publisher<geometry_msgs::msg::PointStamped>("point",10);
+        timer_=this->create_wall_timer(1s,std::bind(&PointBroadcaster::on_timer,this));
+    }
+private:
+    void on_timer()
+    {
+        geometry_msgs::msg::PointStamped ps;
+        ps.header.stamp=this->now();
+        ps.header.frame_id="laser";
+        x+=0.05;
+        ps.point.x=x;
+        ps.point.y=0.0;
+        ps.point.z=-0.1;
+    }
+    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr point_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    double_t x;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<PointBroadcaster>());
+    rclcpp::shutdown();
+    return 0;
+}
